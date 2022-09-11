e4114b361b5c2f435c87db74335bcbac614ce1d2
Wannaporn D <wannapornj.dea@gmail.com>
2022-09-12 03:21:36 +0700

    fix: update the category controller follow the category model

--- controller/category.js
+++ controller/category.js
@@ -3,45 +3,52 @@ const { Op } = require("sequelize");
 
 const getUserCategory = async (req, res) => {
   const userId = req.user.id;
-  const userCategories = await db.Category.findAll({
-    where: { user_id: userId },
-    attributes: { exclude: ["createdAt", "updatedAt"] },
-  });
-  res.status(200).send(userCategories);
+  try {
+    const userCategories = await db.Category.findAll({
+      where: { user_id: userId, isActive: true },
+      attributes: { exclude: ["createdAt", "updatedAt", "user_id"] },
+    });
+    res.status(200).send(userCategories);
+  } catch (err) {
+    console.log("Error fetching categories :>> ", err);
+  }
 };
 const updateUserCategory = async (req, res) => {
   const userId = req.user.id;
-  const { categoryId, name, type } = req.body;
+  const { categoryId, name, type, isActive } = req.body;
   if (!(type.toUpperCase() === "EXPENSE" || type.toUpperCase() === "INCOME")) {
     res.status(400).send({ message: "Invalid category type" });
     return;
   }
-  const targetCate = await db.Category.findOne({
-    where: {
-      [Op.and]: [{ id: categoryId, user_id: userId }],
-    },
-  });
-  if (!targetCate) {
-    res.status(400).send({ message: "No category found" });
-    return;
-  }
-  if (name && type && targetCate.name !== name && targetCate.type !== type) {
-    await targetCate.update({ name, type });
-    res.status(200).send({ message: "Updated category name and type" });
-  } else if (name && targetCate.name !== name) {
-    await targetCate.update({ name });
-    res.status(200).send({ message: "Updated category name" });
-  } else if (type && targetCate.type !== type) {
-    await targetCate.update({ type });
-    res.status(200).send({ message: "Updated category type" });
-  } else {
-    res.status(200).send({ message: "Invalid category name or type" });
+  try {
+    const targetCate = await db.Category.findOne({
+      where: {
+        [Op.and]: [{ id: categoryId, user_id: userId }],
+      },
+    });
+    if (!targetCate) {
+      res.status(400).send({ message: "No category found" });
+      return;
+    }
+    const data = {
+      name: name || targetCate.name,
+      type: type || targetCate.type,
+      isActive: isActive,
+    };
+    try {
+      await targetCate.update(data);
+      res.status(200).send({ message: "Updated successfully" });
+    } catch (err) {
+      console.log("error updating category :>> ", err);
+    }
+  } catch (err) {
+    console.log("error updating category :>> ", err);
   }
 };
 
 const createUserCategory = async (req, res) => {
   const userId = req.user.id;
-  const { name, type } = req.body;
+  const { name, type, isActive } = req.body;
   if (!(name && type)) {
     res.status(400).send({ message: "Invalid category name or type" });
     return;
@@ -50,12 +57,20 @@ const createUserCategory = async (req, res) => {
     res.status(400).send({ message: "Invalid category type" });
     return;
   }
-  const category = await db.Category.findOne({ where: { name } });
-  if (category) {
-    res.status(200).send({ message: "Category already exist" });
-    return;
+  try {
+    const category = await db.Category.findOne({ where: { name } });
+    if (category) {
+      res.status(200).send({ message: "Category already exist" });
+      return;
+    }
+    try {
+      await db.Category.create({ name, type, isActive, user_id: userId });
+      res.status(201).send({ message: "category created successfully" });
+    } catch (err) {
+      console.log("Error creating category :>> ", err);
+    }
+  } catch (err) {
+    console.log("Error finding category in creating category :>> ", err);
   }
-  await db.Category.create({ name, type, user_id: userId });
-  res.status(201).send({ message: "category created successfully" });
 };
 module.exports = { getUserCategory, createUserCategory, updateUserCategory };
