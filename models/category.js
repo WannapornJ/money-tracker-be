a10f1e1ec683aebe6250c7eaa9183968a00f3531
Wannaporn D <wannapornj.dea@gmail.com>
2022-09-12 03:17:41 +0700

    feat: update category model

--- models/category.js
+++ models/category.js
@@ -8,6 +8,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.ENUM('expense', 'income'),
         allowNull: false,
         defaultValue: 'expense'
+      },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
       }
     })
   
